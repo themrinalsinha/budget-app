1688ce5ec683d0ce9699dcbf9fa949754a85f868
Mrinal Sinha <themrinalsinha@gmail.com>
2019-02-08 20:19:02 +0530

    budget display ready

--- budget/models.py
+++ budget/models.py
@@ -22,7 +22,7 @@ class Category(models.Model):
         return self.name
 
 class Expense(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='expenses')
     title   = models.CharField(max_length=100)
     amount  = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
--- budget/views.py
+++ budget/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 
 def project_list(request):
     return render(request, 'project-list.html', {})
 
 def project_detail(request, project_slug):
-    return render(request, 'project-detail.html', {})
-
+    project = get_object_or_404(Project, slug=project_slug)
+    return render(request, 'project-detail.html', {'project': project})
